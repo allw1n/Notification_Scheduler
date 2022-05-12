7cc778dfbc81958e360e43f1684c6de304bd46ca
allw1n <charlesallwin31@gmail.com>
2022-05-12 17:10:19 +0530

    implemented Executor

--- app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
+++ app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -6,8 +6,20 @@ import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class NotificationJobService extends JobService {
 
@@ -39,6 +51,35 @@ public class NotificationJobService extends JobService {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+
+        Runnable displaySleepToast = new Runnable() {
+            @Override
+            public void run() {
+                Log.d("Executor slept for", "10 seconds");
+                Toast.makeText(NotificationJobService.this,
+                        "Executor slept for 10 seconds.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        ScheduledFuture<?> displaySleepHandle = service.scheduleWithFixedDelay(
+                displaySleepToast, 10, 10, TimeUnit.SECONDS);
+
+        Runnable canceller = new Runnable() {
+            @Override
+            public void run() {
+                displaySleepHandle.cancel(false);
+                Log.d("displaySleepHandle", "cancelled after 10 seconds");
+                notificationManager.cancelAll();
+                Log.d("Notification cancelled", " after 10 seconds");
+                jobFinished(jobParameters, false);
+                Log.d("Job finished", " after 10 seconds");
+            }
+        };
+
+        service.schedule(canceller, 12, TimeUnit.SECONDS);
 
         return false;
     }
